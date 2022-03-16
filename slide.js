8e4351f61ac3abf95872ecd07a6360686c53740b
Naek Rizki Sitohang <101006194+NaekRizkiSitohang@users.noreply.github.com>
2022-03-17 01:06:35 +0700

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,74 @@
+var timeOut = 2000;
+var slideIndex = 0;
+var autoOn = true;
+
+autoSlides();
+
+function autoSlides() {
+    timeOut = timeOut - 20;
+
+    if (autoOn == true && timeOut < 0) {
+        showSlides();
+    }
+    setTimeout(autoSlides, 20);
+}
+
+function prevSlide() {
+
+    timeOut = 2000;
+
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex--;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    if (slideIndex == 0) {
+        slideIndex = 3
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function showSlides() {
+
+    timeOut = 2000;
+
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+var today = new Date();
+
+var day = today.getDate();
+var month = today.getMonth()+1; //Januar is 0!
+var year = today.getFullYear();
+var week = today.getDay();
+
+var monthes = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+var weekday= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+
+document.querySelector(".date__month").innerHTML = monthes[month -1];
+document.querySelector(".date__day").innerHTML = day;
+document.querySelector(".date__week").innerHTML = weekday[week];
